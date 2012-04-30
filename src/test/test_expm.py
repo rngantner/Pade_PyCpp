9506370b6663690e1ed05226d85ddae1064da552
Robert Gantner <rngantner@gmail.com>
2012-04-30 13:39:40 +0200

    tab -> space

--- src/test/test_expm.py
+++ src/test/test_expm.py
@@ -13,28 +13,28 @@ import expmat as e
 
 from numpy.testing import assert_almost_equal
 class TestTypes:
-	def helper(self,T,prec=10):
-		Nvals = [10, 50, 100, 200]
-		for n in Nvals:
-			A = array(random.random((n,n)), dtype=T)
-			A = A/norm(A)
-			v = ones((n,1), dtype=T)
-			# C++ version
-			expA = zeros_like(A)
-			e.expmc(A,expA)
-			# assertion
-			assert_almost_equal(expm(A),expA,prec)
+    def helper(self,T,prec=10):
+        Nvals = [10, 50, 100, 200]
+        for n in Nvals:
+            A = array(random.random((n,n)), dtype=T)
+            A = A/norm(A)
+            v = ones((n,1), dtype=T)
+            # C++ version
+            expA = zeros_like(A)
+            e.expmc(A,expA)
+            # assertion
+            assert_almost_equal(expm(A),expA,prec)
 
-	def test_float32(self):
-		self.helper(float32,6)
-	
-	def test_float64(self):
-		self.helper(float64)
-	
-	def test_complex64(self):
-		self.helper(complex64,6)
-	
-	def test_complex128(self):
-		self.helper(complex128)
+    def test_float32(self):
+        self.helper(float32,6)
+    
+    def test_float64(self):
+        self.helper(float64)
+    
+    def test_complex64(self):
+        self.helper(complex64,6)
+    
+    def test_complex128(self):
+        self.helper(complex128)
 
 
